fd0fd6abab68f5d7969bd0a2e06a04cf1b268675
patrick souza <patrickrsmiranda@gmail.com>
2021-01-12 13:21:48 -0400

    add projectController.js

---
+++ backend/src/controllers/projectController.js
@@ -0,0 +1,17 @@
+const connection = require('../database/connection');
+
+
+module.exports ={
+    async index(request,response){
+
+    },
+    async create(request,response){
+
+    },
+    async update(request,response){
+
+    },
+    async delete(request,response){
+
+    }
+}
