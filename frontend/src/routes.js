f05f1d6c9c977b1868c48130e81d9f49b513c58a
patrick souza <patrickrsmiranda@gmail.com>
2021-01-12 14:46:09 -0400

    add pages/NewProject

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+//Pages
 import Home from '../src/pages/Home';
+import NewProject from '../src/pages/NewProject';
 
 export default function Routes() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/new" component={NewProject} />
+            </Switch>
+        </BrowserRouter>
+    )
 }
