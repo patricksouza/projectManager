d1bce912d296b871b28e25e9e9185e9fe9405b1c
patrick souza <patrickrsmiranda@gmail.com>
2021-01-12 17:11:49 -0400

    tratando o retorno da função index

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -2,8 +2,27 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response) {
-        const project = await connection('project')
+        const data = await connection('project')
             .select('*');
+        
+        
+        var project = data.map((item,key,array)=>{
+            let s_date = new Date(item.start_date);
+            var start_date = ((s_date.getDate() )) + "/" + ((s_date.getMonth() + 1)) + "/" + s_date.getFullYear();
+
+            let f_date = new Date(item.finish_date);
+            var finish_date = ((f_date.getDate() )) + "/" + ((f_date.getMonth() + 1)) + "/" + f_date.getFullYear();
+
+            return {
+                id:item.id,
+                name:item.name,
+                start_date: start_date,
+                finish_date:finish_date,
+                budget:item.budget,
+                project_risk:item.project_risk,
+                participants:item.participants
+            }
+        });
         return response.json(project);
     },
     async create(request, response) {
@@ -15,9 +34,6 @@ module.exports = {
             project_risk,
             participants
         } = request.body;
-
-        console.log(request.body);
-
         var participants_name = participants.map((item) => {
             return item;
         });
